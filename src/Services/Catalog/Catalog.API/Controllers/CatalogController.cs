d8853c6a55c43f5d7d8536c14b261d9a473a0f0f
Alexandru Topala <alexandru.topala@flanco.ro>
2023-10-29 21:37:22 +0200

    Lesson 22

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -19,7 +19,7 @@ namespace Catalog.API.Controllers
         public CatalogController(IProductRepository repository, ILogger<CatalogController> logger)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger);
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpGet]
